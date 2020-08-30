caa5fc9a233b7be1fe7776a21b80c9dd4a2f9ab9
Nick Stenquist <whiteshadow148@gmail.com>
2020-08-30 11:42:07 -0600

    Created temple pages and added instructors code spinets
    video 3

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+
+post = [
+    {
+        'author': 'Nick',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_post': 'August 30, 2020'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_post': 'September 1, 2020'
+    }
+]
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': post
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
