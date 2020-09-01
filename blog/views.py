040cfbb5941049be87fe5a63ba9454e0c49f1924
Nick Stenquist <whiteshadow148@gmail.com>
2020-09-01 14:41:49 -0600

    created user registration page
    video 6

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,26 @@
 from django.shortcuts import render
+from .models import Post
 # from django.http import HttpResponse
 
-post = [
-    {
-        'author': 'Nick',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_post': 'August 30, 2020'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_post': 'September 1, 2020'
-    }
-]
+# post = [
+#     {
+#         'author': 'Nick',
+#         'title': 'Blog Post 1',
+#         'content': 'First post content',
+#         'date_post': 'August 30, 2020'
+#     },
+#     {
+#         'author': 'Jane Doe',
+#         'title': 'Blog Post 2',
+#         'content': 'Second post content',
+#         'date_post': 'September 1, 2020'
+#     }
+# ]
 
 # Create your views here.
 def home(request):
     context = {
-        'posts': post
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
